bf964ca07109ff2519baa2fbca2a7d5bed36623b
vitrokb <victor.brns2@gmail.com>
2025-02-24 13:10:27 -0300

    chore: add correct colors for typography

--- src/components/TableHeader/TableHeader.tsx
+++ src/components/TableHeader/TableHeader.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const TableHeader = styled.h2`
   font-size: ${(props) => props.theme.fontSize.md};
   font-weight: ${(props) => props.theme.fontWeight.regular};
+  color: ${(props) => props.theme.colors.white};
 `;
 
 export default TableHeader;
